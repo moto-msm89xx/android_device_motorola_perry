8928401097bd71f39a72f6d0f6d98c66e0b32527
flagersgit <flagersmail@gmail.com>
2020-11-12 10:38:12 -0500

    perry: Import thermal config from stock

--- device.mk
+++ device.mk
@@ -77,8 +77,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-retcn.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-retcn.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Touch
 PRODUCT_PACKAGES += \
